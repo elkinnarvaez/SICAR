69806101f4b8d04771f16545c4bdaa69d8c01cd0
juanjo44 <juanjosebetancourtosorio@gmail.com>
2021-09-28 16:34:56 -0500

    Lista de cultivos agregados

---
+++ SICAR/SICAR/Crop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SICAR
+{
+    public class Crop
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
