908f4c7ee854dc032a51330f33db50f9c77b2ff5
Elkin Narvaez <elkinnarvaez2406@gmail.com>
2021-10-03 18:59:36 -0500

    Binding working

---
+++ SICAR/SICAR/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,22 @@
+﻿using SICAR.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using System.Windows.Input;
+using Xamarin.Essentials;
+
+
+namespace SICAR.ViewModels
+{
+    public class SignUpViewModel : BaseViewModel
+    {
+        public ICommand AlreadyHaveAccountTapped => new Command(async () => await Shell.Current.GoToAsync($"//{nameof(LoginPage)}"));
+
+        public SignUpViewModel()
+        {
+
+        }
+    }
+}
--- SICAR/SICAR/Views/SignUpPage.xaml.cs
+++ SICAR/SICAR/Views/SignUpPage.xaml.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using SICAR.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,7 @@ namespace SICAR.Views
         public SignUpPage()
         {
             InitializeComponent();
+            this.BindingContext = new SignUpViewModel();
         }
     }
 }
