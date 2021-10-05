78a5fb2031851d0780ca199b26295f5979e6d1f7
Elkin Narvaez <elkinnarvaez2406@gmail.com>
2021-10-05 16:48:07 -0500

    Login and SignUp working properly. TODO: store data in Google Drive

--- SICAR/SICAR/ViewModels/LoginViewModel.cs
+++ SICAR/SICAR/ViewModels/LoginViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
 using System.Windows.Input;
+using SICAR.Models;
+using System.Threading.Tasks;
 
 namespace SICAR.ViewModels
 {
@@ -28,11 +30,69 @@ namespace SICAR.ViewModels
 
         public Command LoginCommand { get; }
 
+        private async Task<int> ValidateLogin()
+        {
+            int loginErrorCode;
+            if (!String.IsNullOrWhiteSpace(username) && !String.IsNullOrWhiteSpace(password))
+            {
+                List<User> users = await App.Database.GetUsersAsync();
+                bool usernameFound = false;
+                bool passwordIsCorrect = true;
+                foreach (User user in users)
+                {
+                    if (user.username == username)
+                    {
+                        usernameFound = true;
+                        if (user.password != password)
+                        {
+                            passwordIsCorrect = false;
+                        }
+                    }
+                }
+                if (usernameFound == false)
+                {
+                    loginErrorCode = 1;
+                }
+                else if(passwordIsCorrect == false)
+                {
+                    loginErrorCode = 2;
+                }
+                else
+                {
+                    loginErrorCode = -1;
+                }
+            }
+            else
+            {
+                loginErrorCode = 0;
+            }
+            return loginErrorCode;
+        }
+
         private async void OnLoginClicked(object obj)
         {
             Console.WriteLine(username);
             Console.WriteLine(password);
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            int loginErrorCode = await ValidateLogin();
+            if (loginErrorCode == -1)
+            {
+                Username = "";
+                Password = "";
+                // TODO: need to save the user info for further user in the application
+                await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            }
+            else if (loginErrorCode == 0)
+            {
+                await Shell.Current.DisplayAlert("Campos en blanco", "Por favor verifique que todos los campos estén llenos.", "OK");
+            }
+            else if(loginErrorCode == 1)
+            {
+                await Shell.Current.DisplayAlert("Usuario no existe", "El nombre de usuario ingresado no existe.", "OK");
+            }
+            else
+            {
+                await Shell.Current.DisplayAlert("Contraseña incorrecta", "La contraseña ingresada es incorrecta.", "OK");
+            }
         }
 
         public LoginViewModel()
--- SICAR/SICAR/ViewModels/SignUpViewModel.cs
+++ SICAR/SICAR/ViewModels/SignUpViewModel.cs
@@ -6,7 +6,9 @@ using System.Text;
 using Xamarin.Forms;
 using System.Windows.Input;
 using Xamarin.Essentials;
-
+using SICAR.Models;
+using System.Threading.Tasks;
+using Xamarin.Forms.Xaml;
 
 namespace SICAR.ViewModels
 {
@@ -52,6 +54,44 @@ namespace SICAR.ViewModels
 
         public Command SignUpCommand { get; }
 
+        private async Task<int> ValidateSignUp()
+        {
+            int signUpErrorCode;
+            if(!String.IsNullOrWhiteSpace(names) && !String.IsNullOrWhiteSpace(lastnames) && !String.IsNullOrWhiteSpace(username) && !String.IsNullOrWhiteSpace(password) && !String.IsNullOrWhiteSpace(retypedPassword))
+            {
+                List<User> users = await App.Database.GetUsersAsync();
+                bool usernameFound = false;
+                foreach(User user in users)
+                {
+                    if(user.username == username)
+                    {
+                        usernameFound = true;
+                    }
+                }
+                if (usernameFound == true)
+                {
+                    signUpErrorCode = 1;
+                }
+                else if (password != retypedPassword)
+                {
+                    signUpErrorCode = 2;
+                }
+                else if (password.Length < 6)
+                {
+                    signUpErrorCode = 3;
+                }
+                else
+                {
+                    signUpErrorCode = -1;
+                }
+            }
+            else
+            {
+                signUpErrorCode = 0;
+            }
+            return signUpErrorCode;
+        }
+
         private async void OnSignUpClicked(object obj)
         {
             Console.WriteLine(names);
@@ -59,7 +99,40 @@ namespace SICAR.ViewModels
             Console.WriteLine(username);
             Console.WriteLine(password);
             Console.WriteLine(retypedPassword);
-            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+            int signUpErrorCode = await ValidateSignUp();
+            if(signUpErrorCode == -1)
+            {
+                User newUser = new User()
+                {
+                    username = username,
+                    password = password,
+                    names = names,
+                    lastnames = lastnames
+                };
+                await App.Database.SaveUserAsync(newUser);
+                Names = "";
+                Lastnames = "";
+                Username = "";
+                Password = "";
+                RetypedPassword = "";
+                await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+            }
+            else if (signUpErrorCode == 0)
+            {
+                await Shell.Current.DisplayAlert("Campos en blanco", "Por favor verifique que todos los campos estén llenos.", "OK");
+            }
+            else if(signUpErrorCode == 1)
+            {
+                await Shell.Current.DisplayAlert("Usuario existente", "El nombre de usuario ingresado ya existe.", "OK");
+            }
+            else if(signUpErrorCode == 2)
+            {
+                await Shell.Current.DisplayAlert("Contraseñas no coinciden", "Las contraseñas ingresadas no coinciden.", "OK");
+            }
+            else
+            {
+                await Shell.Current.DisplayAlert("Contraseña muy corta", "La contraseña ingresada es muy corta. Esta debe ser de al menos 6 caracteres.", "OK");
+            }
         }
 
         public SignUpViewModel()
